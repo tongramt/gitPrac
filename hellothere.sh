2f5c95ee35d1dd18f5bb69990d47ebcc94abd66c
Tom Grant <grantth@tcd.ie>
2025-05-27 12:19:48 +0100

    adding shell scripts

---
+++ hellothere.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/bash
+First_Name=Tom
+Second_Name=Grant
+echo $First_Name $Second_Name
---
+++ interactiveshell.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+echo What is your first name?
+read First
+echo What is your surname?
+read Surname
+echo Hello $First $Surname
